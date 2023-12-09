52e0cef76914c0fa6c708aed4e2ebdda83110b5e
Jason Thomo <jthomo@uvic.ca>
2023-12-09 01:10:08 -0800

    day 9

---
+++ day9/SequenceCalculator.java
@@ -0,0 +1,49 @@
+package day9;
+import java.io.*;
+import java.util.*;
+
+public class SequenceCalculator {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("day9/in.txt");
+            Scanner scanner = new Scanner(file);
+            int sum = 0;
+
+            while (scanner.hasNextLine()) {
+                String[] parts = scanner.nextLine().split(" ");
+                List<Integer> seq = new ArrayList<>();
+                for (String part : parts)
+                    seq.add(Integer.parseInt(part));
+                    
+                // sum += nextVal1(seq);  // part 1
+                sum += nextVal2(seq);  // part 2
+            }
+            scanner.close();
+
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Integer> nextSeq(List<Integer> seq) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < seq.size(); i++)
+            result.add(seq.get(i) - seq.get(i - 1));
+        return result;
+    }
+
+    private static int nextVal1(List<Integer> seq) {
+        if (seq.stream().allMatch(v -> v == 0))
+            return 0;
+        return seq.get(seq.size() - 1) + nextVal2(nextSeq(seq));
+    }
+
+    private static int nextVal2(List<Integer> seq) {
+        if (seq.stream().allMatch(v -> v == 0))
+            return 0;
+        return seq.get(0) - nextVal2(nextSeq(seq));
+    }
+}
